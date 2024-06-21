2a66b9dfb26f0820fafe1b30de8a85d16e4e9dac
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-06-21 15:20:51 -0700

    functional buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { callAPI } from "./utils/DataServices";
 import ModalComponent from "./Components/ModalComponent";
 
 export default function Home() {
+  const [displayedCharNames, setDisplayedCharNames] = useState<ICharacterData[]>();
+
   const [allCharNames, setAllCharNames] = useState<ICharacterData[]>();
   const [displayNumStart, setDisplayNumStart] = useState<number>(0);
   const [displayNumEnd, setDisplayNumEnd] = useState<number>(19);
@@ -14,6 +16,7 @@ export default function Home() {
   const saveData = async () => {
     const data: ICharacterData[] = await callAPI();
     setAllCharNames(data);
+    setDisplayedCharNames(data);
   }
 
   const forwardList = async () => {
@@ -27,6 +30,47 @@ export default function Home() {
     if (allCharNames && displayNumStart > 0) {
       setDisplayNumStart(displayNumStart - 20);
       setDisplayNumEnd(displayNumEnd - 20);
+    }
+  }
+
+  const swapList = async () => {
+    let flippedList: ICharacterData[] = [];
+
+    if (displayedCharNames) {
+      for (let i = displayedCharNames.length - 1; i >= 0; i--) {
+        flippedList.push(displayedCharNames[i])
+      }
+
+      setDisplayedCharNames(flippedList);
+    }
+  }
+
+  const standardOrder = async () => {
+    if (allCharNames) {
+      setDisplayedCharNames(allCharNames);
+    }
+  }
+
+  const alphabeticalOrder = async () => {
+    if (allCharNames) {
+      let alphaList: ICharacterData[] = [];
+
+      let allNames: string[] = allCharNames.map(char => {
+        return char.name + "_" + char.id;
+      })
+
+      allNames = allNames.sort();
+
+      allNames.map(name => {
+        allCharNames.map(char => {
+          let nameData = name.split("_");
+          if (char.name === nameData[0] && char.id === nameData[1]) {
+            alphaList.push(char);
+          }
+        })
+      })
+
+      setDisplayedCharNames(alphaList);
     }
   }
 
@@ -44,29 +88,27 @@ export default function Home() {
             <button onClick={backwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Go Back 20</button>
             <button onClick={forwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Go Forward 20</button>
 
-            <button onClick={forwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Switch Direction</button>
+            <button onClick={swapList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Switch Direction</button>
 
-            <button onClick={forwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Sort List By Standard Order</button>
-            <button onClick={forwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Sort List By Alphabetical Order</button>
+            <button onClick={standardOrder} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Sort List By Standard Order</button>
+            <button onClick={alphabeticalOrder} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Sort List By Alphabetical Order</button>
           </div>
         </div>
 
         <div className="flex justify-center">
           <div className="grid grid-cols-2 text-center pb-10 w-screen max-w-[768px]">
             {
-              allCharNames ? allCharNames.map((char: ICharacterData, index: number) => {
+              displayedCharNames ? displayedCharNames.map((char: ICharacterData, index: number) => {
                 if (displayNumStart <= index && index <= displayNumEnd) {
                   return (
                     <div key={index} className="col-span-2 sm:col-span-1 flex justify-center">
-                      <div className="py-2 my-1 bg-black bg-opacity-45 rounded-3xl w-52">
-                        <p className="text-white" onClick={() => { setIndiChar(char); setIsModalOpen(true); }}>{char.name}</p>
-                      </div>
+                      <button className="py-2 my-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white" onClick={() => { setIndiChar(char); setIsModalOpen(true); }}>{char.name}</button>
                     </div>
                   )
                 }
               })
                 :
-                <div className="w-screen grid justify-center">
+                <div className="col-span-2 flex justify-center">
                   <div className="py-3 my-1 bg-black bg-opacity-45 rounded-3xl w-52">
                     <p className="text-white">Loading...</p>
                   </div>
