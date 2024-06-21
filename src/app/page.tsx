8b2ecef6128198bfc982409ac11654341e9b7fc3
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-06-21 08:51:29 -0700

    all names display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
 'use client'
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { callAPI } from "./utils/DataServices";
 
 export default function Home() {
+  const [allCharNames, setAllCharNames] = useState<ICharacterData[]>();
 
-  useEffect(()=>{
-    const bruh = callAPI('api/characters');
+  const saveData = async () => {
+    const data: ICharacterData[] = await callAPI('api/characters')
+    setAllCharNames(data);
+  }
+
+  useEffect(() => {
+    saveData();
   }, []);
 
   return (
     <>
       <div>
-        <p>bruh</p>
+        {/* <p>bruh</p> */}
+        {
+          allCharNames && allCharNames.map((chars: ICharacterData, index: number) => {
+            return (
+              <div key={index}>
+                <p>{chars.name}</p>
+              </div>
+            )
+          })
+        }
       </div>
     </>
   );
