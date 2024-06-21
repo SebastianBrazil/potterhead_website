088dd91a3788acb85415e6cec9f813a3449bce27
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-06-21 09:07:52 -0700

    sorted peeps into rows of 20

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,26 @@ import { callAPI } from "./utils/DataServices";
 
 export default function Home() {
   const [allCharNames, setAllCharNames] = useState<ICharacterData[]>();
+  const [displayNumStart, setDisplayNumStart] = useState<number>(0);
+  const [displayNumEnd, setDisplayNumEnd] = useState<number>(19);
 
   const saveData = async () => {
     const data: ICharacterData[] = await callAPI('api/characters')
     setAllCharNames(data);
+  }
+
+  const forwardList = async () => {
+    if (allCharNames && displayNumEnd < allCharNames.length) {
+      setDisplayNumStart(displayNumStart + 20);
+      setDisplayNumEnd(displayNumEnd + 20);
+    }
+  }
+
+  const backwardList = async () => {
+    if (allCharNames && displayNumStart > 0) {
+      setDisplayNumStart(displayNumStart - 20);
+      setDisplayNumEnd(displayNumEnd - 20);
+    }
   }
 
   useEffect(() => {
@@ -18,14 +34,18 @@ export default function Home() {
   return (
     <>
       <div>
-        {/* <p>bruh</p> */}
+        <button onClick={backwardList} className="bg-black text-white">back</button>
+        <button onClick={forwardList} className="mb-10 ml-4 bg-black text-white">foreward</button>
+
         {
           allCharNames && allCharNames.map((chars: ICharacterData, index: number) => {
-            return (
-              <div key={index}>
-                <p>{chars.name}</p>
-              </div>
-            )
+            if(displayNumStart <= index && index <= displayNumEnd){
+              return (
+                <div key={index}>
+                  <p>{chars.name}</p>
+                </div>
+              )
+            }
           })
         }
       </div>
