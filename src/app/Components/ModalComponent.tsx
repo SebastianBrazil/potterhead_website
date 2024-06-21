f570ffb0245240d8863f562fdb337a56822a9cce
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-06-21 16:36:52 -0700

    modal styling

--- src/app/Components/ModalComponent.tsx
+++ src/app/Components/ModalComponent.tsx
@@ -1,22 +1,92 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import defaultImage from "@/app/assets/noimeg.png"
 
 const ModalComponent = (props: IModalProps) => {
+    const [image, setImage] = useState<string>();
+    const [altNames, setAltNames] = useState<string>();
+    const [isAlive, setIsAlive] = useState<string>();
+    const [birth, setBirth] = useState<string>();
+    const [wiz, setWiz] = useState<string>();
+
+    useEffect(() => {
+        if (props.char.alternate_names.length > 0) {
+            setAltNames(props.char.alternate_names.join(", "))
+        }
+
+        if (props.char.image !== "") {
+            setImage(props.char.image)
+        } else {
+            setImage(defaultImage.src)
+        }
+
+        if (props.char.alive === true) {
+            setIsAlive("Yes")
+        } else {
+            setIsAlive("No")
+        }
+
+        if (props.char.wizard === true) {
+            setWiz("Yes")
+        } else {
+            setWiz("No")
+        }
+
+        if (props.char.dateOfBirth) {
+            let birthArr = props.char.dateOfBirth.split("-");
+
+            setBirth(birthArr[1] + "/" + birthArr[0] + "/" + birthArr[2]);
+        }
+    }, [])
+
     return (
         <>
-            <div className='fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-20'>
-                <div className='z-10 absolute top-0 left-0 bottom-0 right-0 m-auto max-md:mx-10 max-w-[700px] max-h-[380px] bg-slate-500'>
-                    <div>
-                        <p className='text-white text-center'>{props.char.name}</p>
-                        <p className='text-white text-center'>{props.char.actor}</p>
-                        <p className='text-white text-center'>{props.char.species}</p>
-                        <p className='text-white text-center'>{props.char.gender}</p>
-                        <p className='text-white text-center'>{props.char.wizard}</p>
+            <div className='fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-40'>
+                <div className='z-10 absolute top-0 left-0 bottom-0 right-0 m-auto max-md:mx-10 max-w-[700px] max-h-[500px] rounded-3xl grid grid-rows-12 bg-slate-500'>
+                    <div className='row-span-11 flex flex-col self-center'>
+                        {image &&
+                            <div className='flex justify-center mt-2'>
+                                <img width={150} src={image} alt="Image Of Character" />
+                            </div>
+                        }
+                        <p className='text-white text-center mt-2'>Name: {props.char.name}</p>
+                        {
+                            altNames &&
+                            <p className='text-white text-center'>AKA: {altNames}</p>
+                        }
+                        {
+                            props.char.actor &&
+                            <p className='text-white text-center'>Actor: {props.char.actor}</p>
+                        }
+                        {
+                            props.char.species &&
+                            <p className='text-white text-center'>Species: {props.char.species[0].toUpperCase() + props.char.species.substring(1)}</p>
+                        }
+                        {
+                            props.char.gender &&
+                            <p className='text-white text-center'>Gender: {props.char.gender[0].toUpperCase() + props.char.gender.substring(1)}</p>
+                        }
+                        {
+                            isAlive &&
+                            <p className='text-white text-center'>Are They Alive?: {isAlive}</p>
+                        }
+                        {
+                            birth &&
+                            <p className='text-white text-center'>Birth Date: {birth}</p>
+                        }
+                        {
+                            props.char.ancestry &&
+                            <p className='text-white text-center'>Ancestry: {props.char.ancestry[0].toUpperCase() + props.char.ancestry.substring(1)}</p>
+                        }
+                        {
+                            wiz &&
+                            <p className='text-white text-center'>Are They a Wizard?: {wiz}</p>
+                        }
                     </div>
 
-                    <div>
-                        <p onClick={() => { props.setIsModalOpen(false) }} className=' text-red-300 text-center'>Close</p>
+                    <div className='row-span-1 flex justify-center'>
+                        <p onClick={() => { props.setIsModalOpen(false) }} className='bg-red-500 w-20 h-[25px] rounded-full text-white text-center'>Close</p>
                     </div>
                 </div>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,43 +80,41 @@ export default function Home() {
 
   return (
     <>
-      {isModalOpen && indiChar && <ModalComponent isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} char={indiChar} />}
+      <div className="pt-10 grid grid-cols-1">
+        <div className="max-md:grid pb-10 md:pb-40 md:flex justify-center">
+          <button onClick={backwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Go Back 20</button>
+          <button onClick={forwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Go Forward 20</button>
 
-      <div className="w-screen h-screen">
-        <div className="pt-10 grid grid-cols-1">
-          <div className="max-md:grid pb-10 md:pb-40 md:flex justify-center">
-            <button onClick={backwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Go Back 20</button>
-            <button onClick={forwardList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Go Forward 20</button>
+          <button onClick={swapList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Switch Direction</button>
 
-            <button onClick={swapList} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Switch Direction</button>
-
-            <button onClick={standardOrder} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Sort List By Standard Order</button>
-            <button onClick={alphabeticalOrder} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Sort List By Alphabetical Order</button>
-          </div>
-        </div>
-
-        <div className="flex justify-center">
-          <div className="grid grid-cols-2 text-center pb-10 w-screen max-w-[768px]">
-            {
-              displayedCharNames ? displayedCharNames.map((char: ICharacterData, index: number) => {
-                if (displayNumStart <= index && index <= displayNumEnd) {
-                  return (
-                    <div key={index} className="col-span-2 sm:col-span-1 flex justify-center">
-                      <button className="py-2 my-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white" onClick={() => { setIndiChar(char); setIsModalOpen(true); }}>{char.name}</button>
-                    </div>
-                  )
-                }
-              })
-                :
-                <div className="col-span-2 flex justify-center">
-                  <div className="py-3 my-1 bg-black bg-opacity-45 rounded-3xl w-52">
-                    <p className="text-white">Loading...</p>
-                  </div>
-                </div>
-            }
-          </div>
+          <button onClick={standardOrder} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Sort List By Standard Order</button>
+          <button onClick={alphabeticalOrder} className="py-3 m-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white">Sort List By Alphabetical Order</button>
         </div>
       </div>
+
+      <div className="flex justify-center">
+        <div className="grid grid-cols-2 text-center pb-10 w-screen max-w-[768px]">
+          {
+            displayedCharNames ? displayedCharNames.map((char: ICharacterData, index: number) => {
+              if (displayNumStart <= index && index <= displayNumEnd) {
+                return (
+                  <div key={index} className="col-span-2 sm:col-span-1 flex justify-center">
+                    <button className="py-2 my-1 bg-black bg-opacity-45 rounded-3xl w-52 text-white" onClick={() => { setIndiChar(char); setIsModalOpen(true); }}>{char.name}</button>
+                  </div>
+                )
+              }
+            })
+              :
+              <div className="col-span-2 flex justify-center">
+                <div className="py-3 my-1 bg-black bg-opacity-45 rounded-3xl w-52">
+                  <p className="text-white">Loading...</p>
+                </div>
+              </div>
+          }
+        </div>
+      </div>
+
+      {isModalOpen && indiChar && <ModalComponent isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} char={indiChar} />}
     </>
   );
 }
